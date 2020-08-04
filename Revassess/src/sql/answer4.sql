20f69c77ee500fc02a0939b5ca4895d800187ad3
thecheatisgod@gmail.com <thecheatisgod@gmail.com>
2020-08-04 14:35:08 -0600

    another try

--- Revassess/src/sql/answer4.sql
+++ Revassess/src/sql/answer4.sql
@@ -1,5 +1,5 @@
-SELECT app_user.username, category.name, flashcard.answer, flashcard.answer  FROM study_set
- INNER JOIN study_set ON app_user.user_id = study_set.owner_id
+SELECT app_user.username, category.name, flashcard.question, flashcard.answer  FROM study_set
+ INNER JOIN app_user ON study_set.owner_id = app_user.user_id
  INNER JOIN study_set_card ON study_set.study_set_id = study_set_card.study_set_id
  INNER JOIN flashcard ON study_set_card.flashcard_id = flashcard.flashcard_id
  INNER JOIN category ON flashcard.category_id = category.category_id
