45c362ed827ec94660bc029f635d55990df92e04
thecheatisgod@gmail.com <thecheatisgod@gmail.com>
2020-08-04 13:34:09 -0600

    done

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -1,8 +1,29 @@
 package com.revature.tier1;
 
+import java.util.ArrayList;
+
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+
+		int power = 0;
+		ArrayList<Integer> digits = new ArrayList<>();
+		String parsedLong = Long.toString(num);
+
+		for (int x = 0; x < parsedLong.length(); x++) {
+			digits.add(Integer.parseInt(parsedLong.substring(x, x+1)));
+		}
+		int total = 0;
+		power = parsedLong.length();
+
+		for (Integer i : digits) {
+
+			total += i ^ power;
+		}
+
+		if (total == num) {
+			return true;
+		} else
+			return false;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,16 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int sum = 0;
+		if (arr == null) {
+			return 0;
+		} else {
+			for(int i : arr) {
+				sum += i;
+			}
+			return sum;
+		}
+			
+
 	}
 }
